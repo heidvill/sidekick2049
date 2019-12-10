39de8f69bef512e4ca9e1b9bda24e1660439621a
miikkalintula <miikkalintula@gmail.com>
2019-12-10 11:44:22 +0200

    Facebook-autentikaatio lisätty

--- SideKickMVC/Startup.cs
+++ SideKickMVC/Startup.cs
@@ -28,6 +28,11 @@ namespace SideKickMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
