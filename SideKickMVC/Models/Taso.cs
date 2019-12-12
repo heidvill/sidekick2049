4f77df8de70109ca84366ef003dc71fffeec7c4a
Marko Juvonen <marko.tt.juvonen@gmail.com>
2019-12-12 14:32:25 +0200

    ulkoasumuutoksia

---
+++ SideKickMVC/Models/Taso.cs
@@ -0,0 +1,22 @@
+﻿using SideKickDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SideKickMVC.Models
+{
+    public class Taso
+    {
+        public Tilasto Tilasto { get; set; }
+        public static Dictionary<int, string> Linkit { get; set; } = new Dictionary<int, string>();
+
+        static Taso()
+        {
+            Linkit.Add(1, "Kulkukortti");
+            Linkit.Add(2, "Lista");
+            Linkit.Add(3, "Color_It_Redd");
+            Linkit.Add(4, "Morse");
+        }
+    }
+}
