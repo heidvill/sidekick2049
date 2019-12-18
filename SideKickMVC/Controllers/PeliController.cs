286be193712d96cab42ac4d0b224b05a8f6f3bd4
Marko Juvonen <marko.tt.juvonen@gmail.com>
2019-12-18 11:40:05 +0200

    Levysoittimen randomointi

--- SideKickMVC/Controllers/PeliController.cs
+++ SideKickMVC/Controllers/PeliController.cs
@@ -101,7 +101,7 @@ namespace SideKickMVC.Controllers
             {
                 return View();
             }
-            else if (korttikoodi.Trim().ToLower() == Tehtavat.KulkukorttiVastaukset[random])
+            else if (korttikoodi.Trim().ToLower() == Tehtavat.kulkukorttiVastaukset[random])
             {
                 TallennaTietokantaan(1);
                 return RedirectToAction("Lista").WithSuccess("Hienoa!", "Oikea vastaus");
@@ -197,7 +197,7 @@ namespace SideKickMVC.Controllers
             {
                 return View();
             }
-            else if (salasana.Trim().ToLower() == Tehtavat.MorseVastaukset[random])
+            else if (salasana.Trim().ToLower() == Tehtavat.morseVastaukset[random])
             {
                 TallennaTietokantaan(4);
                 return RedirectToAction("Labyrintti").WithSuccess("Hienoa!", "Oikea vastaus");
@@ -245,13 +245,13 @@ namespace SideKickMVC.Controllers
         }
 
         [HttpPost]
-        public IActionResult Levysoitin(string albumi)
+        public IActionResult Levysoitin(string albumi, int random)
         {
             if (string.IsNullOrEmpty(albumi))
             {
                 return View();
             }
-            else if (albumi.Trim().ToLower() == "looking for freedom")
+            else if (albumi.Trim().ToLower() == Tehtavat.lsVastaukset[random])
             {
                 TallennaTietokantaan(6);
                 return RedirectToAction("Portaikko").WithSuccess("Hienoa!", "Oikea vastaus");
--- SideKickMVC/Models/Tehtavat.cs
+++ SideKickMVC/Models/Tehtavat.cs
@@ -7,34 +7,46 @@ namespace SideKickMVC.Models
 {
     public class Tehtavat
     {
-        public static Dictionary<int, string> Kulkukortit = new Dictionary<int, string>();
+        public static Dictionary<int, string> kulkukortit = new Dictionary<int, string>();
 
-        public static Dictionary<int, string> KulkukorttiVastaukset = new Dictionary<int, string>();
-        public static Dictionary<int, string> Morsekoodit = new Dictionary<int, string>();
-        public static Dictionary<int, string> MorseVastaukset = new Dictionary<int, string>();
+        public static Dictionary<int, string> kulkukorttiVastaukset = new Dictionary<int, string>();
+        public static Dictionary<int, string> morsekoodit = new Dictionary<int, string>();
+        public static Dictionary<int, string> morseVastaukset = new Dictionary<int, string>();
+        public static Dictionary<int, string> lsTeksti = new Dictionary<int, string>();
+        public static Dictionary<int, string> lsVastaukset = new Dictionary<int, string>();
 
         static Tehtavat()
         {
-            Kulkukortit.Add(1, "../media/accesscard.jpg");
-            Kulkukortit.Add(2, "../media/accesscard2.jpg");
-            Kulkukortit.Add(3, "../media/accesscard3.jpg");
-            Kulkukortit.Add(4, "../media/accesscard4.jpg");
-            Kulkukortit.Add(5, "../media/accesscard5.jpg");
-            KulkukorttiVastaukset.Add(1, "platyrhynchos");
-            KulkukorttiVastaukset.Add(2, "leucopsis");
-            KulkukorttiVastaukset.Add(3, "galericulata");
-            KulkukorttiVastaukset.Add(4, "carolinensis");
-            KulkukorttiVastaukset.Add(5, "merganser");
-            Morsekoodit.Add(1, "../media/piip.wav");
-            Morsekoodit.Add(2, "../media/piip2.wav");
-            Morsekoodit.Add(3, "../media/piip3.wav");
-            Morsekoodit.Add(4, "../media/piip4.wav");
-            Morsekoodit.Add(5, "../media/piip5.wav");
-            MorseVastaukset.Add(1, "pulu");
-            MorseVastaukset.Add(2, "lokki");
-            MorseVastaukset.Add(3, "tikka");
-            MorseVastaukset.Add(4, "kana");
-            MorseVastaukset.Add(5, "lunni");
+            kulkukortit.Add(1, "../media/accesscard.jpg");
+            kulkukortit.Add(2, "../media/accesscard2.jpg");
+            kulkukortit.Add(3, "../media/accesscard3.jpg");
+            kulkukortit.Add(4, "../media/accesscard4.jpg");
+            kulkukortit.Add(5, "../media/accesscard5.jpg");
+            kulkukorttiVastaukset.Add(1, "platyrhynchos");
+            kulkukorttiVastaukset.Add(2, "leucopsis");
+            kulkukorttiVastaukset.Add(3, "galericulata");
+            kulkukorttiVastaukset.Add(4, "carolinensis");
+            kulkukorttiVastaukset.Add(5, "merganser");
+            morsekoodit.Add(1, "../media/piip.wav");
+            morsekoodit.Add(2, "../media/piip2.wav");
+            morsekoodit.Add(3, "../media/piip3.wav");
+            morsekoodit.Add(4, "../media/piip4.wav");
+            morsekoodit.Add(5, "../media/piip5.wav");
+            morseVastaukset.Add(1, "pulu");
+            morseVastaukset.Add(2, "lokki");
+            morseVastaukset.Add(3, "tikka");
+            morseVastaukset.Add(4, "kana");
+            morseVastaukset.Add(5, "lunni");
+            lsTeksti.Add(1, $"One morning in June some twenty years ago I was born a rich man's son");
+            lsTeksti.Add(2, $"Whoa, ain't the moon a beauty man don't it shine");
+            lsTeksti.Add(3, $"Lips as sweet as candy your taste is on my mind");
+            lsTeksti.Add(4, $"I don't even wanna try I guess the both of us sing a foolish lullaby");
+            lsTeksti.Add(5, $"I've been lookin' back and longin' for the freedom from my chains");
+            lsVastaukset.Add(1, "looking for freedom");
+            lsVastaukset.Add(2, "night rocker");
+            lsVastaukset.Add(3, "hooked on a feeling");
+            lsVastaukset.Add(4, "everybody sunshine");
+            lsVastaukset.Add(5, "lovin' feelings");
         }
     }
 }
